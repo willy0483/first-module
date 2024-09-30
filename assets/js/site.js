7198fefd6119a16c59cae95b1b88a9b79edf764b
willy0483 <willyjensen251@gmail.com>
2024-09-30 12:42:18 +0200

    ok

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,5 +1,9 @@
 // du må kun bruge importerede moduler her...
 
 import { myName } from "./modules/myName/myName.js";
+import { myNav } from "./modules/myNav/myNav.js";
+import { getName } from "./modules/getName/getName.js";
 
 myName();
+myNav();
+getName();
